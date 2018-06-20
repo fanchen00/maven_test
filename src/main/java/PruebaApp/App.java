795bf8339a86c5bdf972542cedf97e4a92aced54
pengwenjuan <877551291@qq.com>
2018-06-20 17:49:55 +0800

    啦啦啦啦

--- src/main/java/PruebaApp/App.java
+++ src/main/java/PruebaApp/App.java
@@ -6,9 +6,9 @@ package PruebaApp;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "first test" );
- //   }
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
+        System.out.println("first test");
+        System.out.println("Que pedo");
+    }
 }
